39dbd28871a0d80433df4f00beccdab9efdff394
pluricalifornia <pluricalifornia@gmail.com>
2025-03-18 22:41:57 -0700

    Create reactapp.js

---
+++ src/reactapp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CLDGraph from "./CLDGraph";
+
+function App() {
+  return (
+    <div className="App">
+      <CLDGraph />
+    </div>
+  );
+}
+
+export default App;
